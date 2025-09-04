3c9b1166fd8be591068d7b64eb4920863163bd0b
nmwei <254060001@qq.com>
2025-09-04 11:48:07 +0800

    Fix image optimization for static export - disable unoptimized images for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -56,7 +56,7 @@ const securityHeaders = [
 
 const output = process.env.EXPORT ? 'export' : undefined
 const basePath = process.env.BASE_PATH || undefined
-const unoptimized = process.env.UNOPTIMIZED ? true : undefined
+const unoptimized = process.env.EXPORT ? true : undefined
 
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
